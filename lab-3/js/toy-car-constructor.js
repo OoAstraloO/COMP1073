a900de16c0d6eaddb55decfc75c5c70c28c24ed4
Charlie <200533963@student.georgianc.on.ca>
2026-03-16 11:58:49 -0400

    Created ToyCar Constructor

---
+++ lab-3/js/toy-car-constructor.js
@@ -0,0 +1,35 @@
+// Toy Car Contrustor
+function ToyCar(
+    brand, 
+    model, 
+    year, 
+    colour, 
+    scale, 
+    manufacturer, 
+    material, 
+    vehicleType, 
+    tireType, 
+    openingParts, 
+    detailedExterior,
+    collectible,
+    price,
+    stock,
+    sku     
+)
+{
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.colour = colour;
+    this.scale = scale;
+    this.manufacturer = manufacturer;
+    this.material = material;
+    this.vehicleType = vehicleType;
+    this.tireType = tireType;
+    this.openingParts = openingParts;
+    this.detailedExterior = detailedExterior;
+    this.collectible = collectible;
+    this.price = price;
+    this.stock = stock;
+    this.sku = sku;
+}
