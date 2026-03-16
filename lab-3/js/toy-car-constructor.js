01e0336fad57d92a28d6df7bfb406f5f687d4689
Charlie <200533963@student.georgianc.on.ca>
2026-03-16 12:10:18 -0400

    Created CreateCar function
    Created function which creates an instance from the form inputs

--- lab-3/js/toy-car-constructor.js
+++ lab-3/js/toy-car-constructor.js
@@ -33,3 +33,46 @@ function ToyCar(
     this.stock = stock;
     this.sku = sku;
 }
+
+let carCollection = [];
+
+// Creates instance from form
+function CreateCar()
+{
+    const brand = document.getElementById("brand").value;
+    const model = document.getElementById("model").value;
+    const year = document.getElementById("year").value;
+    const colour = document.getElementById("colour").value;
+    const scale = document.getElementById("scale").value;
+    const manufacturer = document.getElementById("manufacturer").value;
+    const material = document.getElementById("material").value;
+    const vehicleType = document.getElementById("vehicleType").value;
+    const tireType = document.getElementById("tireType").value;
+    const openingParts = document.getElementById("openingParts").value;
+    const detailedExterior = document.getElementById("detailedExterior").value;
+    const collectible = document.getElementById("collectible").value;
+    const price = document.getElementById("price").value;
+    const stock = document.getElementById("stock").value;
+    const sku = document.getElementById("sku").value;
+
+    const newCar = new ToyCar (
+        brand,
+        model,
+        year,
+        colour,
+        scale, 
+        manufacturer, 
+        material, 
+        vehicleType, 
+        tireType, 
+        openingParts, 
+        detailedExterior,
+        collectible,
+        price,
+        stock,
+        sku 
+    );
+
+    carCollection.push(newCar);
+    displayCars();
+}
