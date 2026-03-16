0ddc47700fbbb5cce66654ffab012a7111a3f713
Charlie <200533963@student.georgianc.on.ca>
2026-03-16 19:14:05 -0400

    Updated form-submission.js
    Updated form submission to use query selectors rather than getElementById, due to buttons being radio buttons

--- assignment-2/js/form-submission.js
+++ assignment-2/js/form-submission.js
@@ -1,17 +1,24 @@
+// Adds an event listener to the form, which runs when the form is submitted
 document.getElementById("bowlForm").addEventListener("submit", function(event)
 {
+    // Prevents the default form submission behaviour
     event.preventDefault();
 
-    const size = document.getElementById("size").value;
-    const base = document.getElementById("base").value;
+    // Gets the values from the form inputs and stores them in variables
+    const size = document.querySelector("input[name='size']:checked").value;
+    const base = document.querySelector("input[name='base']:checked").value;
+    // Creates arrays to store the selected fruits and toppings, poppulating them with the values of the boxes that are checked
     const fruits = [];
     document.querySelectorAll("input[name='fruits']:checked").forEach(input => fruits.push(input.value));
     const toppings = [];
-    document.querySelectorAll("input[name='toppings']:checked").forEach(input => toppings.push(input.value));   
-    const protein = document.getElementById("protein").value;
+    document.querySelectorAll("input[name='toppings']:checked").forEach(input => toppings.push(input.value)); 
+    // Gets the values from the form inputs and stores them in variables
+    const protein = document.querySelector("input[name='proteins']:checked").value;
     const instructions = document.getElementById("instructions").value;
 
+    // Creates a new instance of the EnergyBowl class using the values from the form inputs
     const energyBowl = new EnergyBowl(size, base, fruits, toppings, protein, instructions);
 
+    // Displays the description of the energy bowl in the order summary section of the page
     document.getElementById("orderSummary").innerHTML = energyBowl.getDescription();
 });
