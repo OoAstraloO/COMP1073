1b882546757a0cbe4e7c2307dbe4f5469e7d8353
Charlie <200533963@student.georgianc.on.ca>
2026-03-16 17:14:44 -0400

    Created EnergyBowl constructor
    Created constructor for EnergyBowl which takes 6 various parameters.

---
+++ assignment-2/js/energy-bowl.js
@@ -0,0 +1,12 @@
+class EnergyBowl 
+{
+    constructor(size, base, fruits, toppings, protein, instructions)
+    {
+        this.size = size;
+        this.base = base;
+        this.fruits = fruits;
+        this.toppings = toppings;
+        this.protein = protein;
+        this.instructions = instructions;
+    }
+}
