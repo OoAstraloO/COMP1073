2e416877929fc83dcb3e5423ace5bd4dc8247eea
Charlie <200533963@student.georgianc.on.ca>
2026-04-08 15:43:19 -0400

    Added addToList function
    Added in AddToList function which adds items to the to-do list and will change according to different inputs

--- assignment-3/js/todolist.js
+++ assignment-3/js/todolist.js
@@ -2,3 +2,47 @@ const taskInput = document.getElementById("taskInput");
 const addTask = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
 
+function addToList()
+{
+    const text = taskInput.value.trim();
+
+    if (text === "") return;
+
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    const deleteTask = document.createElement("button")
+    deleteTesk.textContent = "Delete";
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteTask);
+
+    taskList.appendChild(li);
+
+    tastInput.value = "";
+
+    checkbox.addEventListener("change", function()
+    {
+        if (checkbox.checked)
+        {
+            li.classList.add("completed");
+            list.appendChild(li);
+        }
+        else
+        {
+            li.style.textDecoration = "none"
+        }
+    })
+
+    deleteTask.addEventListener("click", function()
+    {
+        li.remove();
+    })
+}
+
